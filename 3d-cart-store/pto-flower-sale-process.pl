4a66297f4bc3b19fc2344805626d1f00038fca35
Jeff Squyres <jeff@squyres.com>
2017-03-28 20:52:43 -0400

    flowers: bring in all the improvements from the SAAM script
    Still somewhat modified for the flowers script.
    
    Signed-off-by: Jeff Squyres <jeff@squyres.com>

--- 3d-cart-store/pto-flower-sale-process.pl
+++ 3d-cart-store/pto-flower-sale-process.pl
@@ -1,12 +1,24 @@
 #!/usr/bin/env perl
 
 # This helper script takes a CSV export of sales from the 3d cart
-# store and parsed out the "options" from the itemname column and add
-# extra rows at the end with those options (in this case, the only
-# option is the color of some of the flowers).
+# store and parsed out the "options" from the itemname column and put
+# them into a new CSV consisting of just these values.
+#
+# Specifically, when someone registers for a sport on the 3d cart
+# store, there's a series of questions that must be answered before
+# the item (i.e., registration) can be added to the cart, such as:
+# athlete name, uniform size, ...etc.  All of these questions get
+# mushed into a single column in the CSV sales report that you can
+# download from the store, making it fairly unintelligible for a human
+# to read.  Plus, the sports ministry doesn't need all the billing
+# information that comes in the CSV sales report -- all they need is
+# the answers to the questions that accompanied the purchase of the
+# sports registration.  This script simply parses out those answers
+# and outputs them into a new CSV that can be easily used by the
+# Sports Ministry to list all the athletes who have registered, etc.
 #
 # The script is currently hard-coded to look for item ID's with
-# "1368.81" in the name.  This should probably be turned into a
+# "2015-SPR" in the name.  This should probably be turned into a
 # command line option.
 
 ###################################################################
@@ -20,37 +32,112 @@ use Data::Dumper;
 die "Must specify file"
     if (!defined($ARGV[0]));
 
+my $order_search_key = "PTO-2017-FLOWERS";
+
 ###################################################################
 
 my $csv = Text::CSV->new();
 
 my $fh;
+print "Reading $ARGV[0]...\n";
 open($fh, $ARGV[0]) ||
     die("Can't open file $ARGV[0]");
 
 # Find the "itemname" column
-my $all_columns;
-$all_columns = $csv->getline($fh);
+my $fields;
+$fields = $csv->getline($fh);
 my $col = 0;
-my $itemname_column = 0;
-my $itemid_column = 0;
-foreach my $field (@$all_columns) {
+my $itemname_column = -1;
+my $itemid_column = -1;
+my $orderid_column = -1;
+my $orderdate_column = -1;
+my $ordertime_column = -1;
+my $orderfirstname_column = -1;
+my $orderlastname_column = -1;
+my $orderemail_column = -1;
+my $orderphone_column = -1;
+
+my $itemquantity_column = -1;
+my $itemprice_column = -1;
+
+foreach my $field (@$fields) {
     $itemname_column = $col
-        if ($field eq "itemname");
+	if ($field eq "itemname");
     $itemid_column = $col
-        if ($field eq "itemid");
+	if ($field eq "itemid");
+    $orderid_column = $col
+	if ($field eq "orderid");
+    $orderdate_column = $col
+	if ($field eq "odate");
+    $ordertime_column = $col
+	if ($field eq "otime");
+    $orderfirstname_column = $col
+	if ($field eq "ofirstname");
+    $orderlastname_column = $col
+	if ($field eq "olastname");
+    $orderemail_column = $col
+	if ($field eq "oemail");
+    $orderphone_column = $col
+	if ($field eq "ophone");
+
+    $itemquantity_column = $col
+	if ($field eq "numitems");
+    $itemprice_column = $col
+	if ($field eq "itemamount");
+
     ++$col;
 }
 
-# Read in the rest of the input
+sub check {
+    my $name = shift;
+    my $value = eval("\$$name");
+    die "Did not find column for $name"
+	if (!defined($value) || $value < 0);
+}
+
+check("orderid_column");
+check("orderdate_column");
+check("ordertime_column");
+check("orderfirstname_column");
+check("orderlastname_column");
+check("orderemail_column");
+check("orderphone_column");
+
+check("itemid_column");
+check("itemname_column");
+check("itemquantity_column");
+check("itemprice_column");
+
+# Read in the rest of the input; save rows that match a given item ID
+# pattern
 my @rows;
 while (my $row = $csv->getline($fh)) {
     push(@rows, $row)
-        if ($row->[$itemid_column] =~ /1368\.81/);
+	if ($row->[$itemid_column] =~ /$order_search_key/);
 }
 close($fh);
 
 ###################################################################
+
+sub normalize_name {
+    my $val = shift;
+
+    $val =~ s/:$//;
+
+    # Return some common field errors that have cropped up over the
+    # years.  E.g., we accidentally put in "Gender" instead of "Sex" a
+    # few times.  So consolidate all "Gender" fields into "Sex".
+    return "Athlete sex"
+	if ($val =~ /^Athlete gender$/i);
+    return "Parent #1 email"
+	if ($val =~ /^Parent \#1.+email/i);
+    return "Parent #2 email (optional)"
+	if ($val =~ /^Parent \#2.+email/i);
+    return "Parent #2 phone number (optional)"
+	if ($val =~ /^Parent \#2.+phone number/i);
+
+    return $val;
+}
 
 sub parse_itemname {
     my $item = shift;
@@ -65,21 +152,28 @@ sub parse_itemname {
     my @columns;
     # There are two forms
     foreach my $field (@fields) {
-        # First form: <b>Field name</b>&nbsp;Label:value
-        if ($field =~ m@<b>(.+?)</b>&nbsp;(.+?):(.+?)$@) {
-            push(@columns, {
-                name => $1,
-                label => $2,
-                value => $3,
-                });
-        }
-        # Second form: <b>Field name:</b>&nbsp;value
-        elsif ($field =~ m@<b>(.+?):</b>&nbsp;(.+?)$@) {
-            push(@columns, {
-                name => $1,
-                value => $2,
-                });
-        }
+	# First form: <b>Field name</b>&nbsp;Label:value
+	if ($field =~ m@<b>(.+?)</b>&nbsp;(.+?):(.+?)$@) {
+	    my $name = normalize_name($1);
+	    my $label = $2;
+	    my $value = $3;
+
+	    push(@columns, {
+		name => $name,
+		label => $label,
+		value => $value,
+		});
+	}
+	# Second form: <b>Field name:</b>&nbsp;value
+	elsif ($field =~ m@<b>(.+?):</b>&nbsp;(.+?)$@) {
+	    my $name = normalize_name($1);
+	    my $value = $2;
+
+	    push(@columns, {
+		name => $name,
+		value => $value,
+		});
+	}
     }
 
     return @columns;
@@ -91,49 +185,62 @@ sub parse_itemname {
 # reported in that row (i.e., that column isn't blank -- it isn't even
 # reported).  Sigh.  So scan through *all* the rows and find the
 # union of *all* the column names.
-my $all_itemname_columns;
+my $all_columns;
 foreach my $row (@rows) {
     my $item = $$row[$itemname_column];
     my @columns = parse_itemname($item);
 
     foreach my $c (@columns) {
-        $all_itemname_columns->{$c->{name}} = 1;
+	$all_columns->{$c->{name}} = 1;
     }
 }
 
 # Open the output CSV
+print "Writing to processed.csv...\n";
 open(OUT, ">processed.csv") ||
     die "Can't write to processed.csv";
 
 # Write the first line of the output CSV: the union of all the column
 # names.
-print OUT join(",", @{$all_columns}) . "," .
-    join(",", sort(keys(%{$all_itemname_columns}))) . "\n";
+print OUT "\"Order ID\",\"Order date\",\"Order time\",\"First name\",\"Last name\",\"Email\",\"Phone number\",\"Item\",\"Item quantity\",\"Item total amount\"," . join(",", sort(keys(%{$all_columns}))) . "\n";
 
 # Now write all the values
 foreach my $row (@rows) {
     my $item = $$row[$itemname_column];
     my @columns = parse_itemname($item);
 
-    print OUT join(',', @{$row}) . ",";
+    # Output all the values.  Always output these first:
+    # Order ID, order date, order timestamp, order item ID
+    print OUT $$row[$orderid_column] . "," .
+	$$row[$orderdate_column] . "," .
+	$$row[$ordertime_column] . "," .
+	$$row[$orderfirstname_column] . "," .
+	$$row[$orderlastname_column] . "," .
+	$$row[$orderemail_column] . "," .
+	$$row[$orderphone_column] . "," .
+	$$row[$itemid_column] . "," .
+	$$row[$itemquantity_column] . "," .
+        $$row[$itemprice_column];
 
-    # Output all the values.  Must go in the same order that we output
-    # the first line.  If we don't have that value, output an empty
-    # column.  :-(
-    foreach my $column (sort(keys(%{$all_itemname_columns}))) {
-        foreach my $c (@columns) {
-            if ($c->{name} eq $column) {
-                my $value = $c->{value};
-                if ($value =~ /,/) {
-                    print OUT "\"$value\"";
-                } else {
-                    print OUT "$value";
-                }
-            }
-            print OUT ",";
-        }
+    # Must go in the same order that we output the first line.  If we
+    # don't have that value, output an empty column.  :-(
+    foreach my $column (sort(keys(%{$all_columns}))) {
+	print OUT ",";
+	foreach my $c (@columns) {
+	    if ($c->{name} eq $column) {
+		my $value = $c->{value};
+		if ($value =~ /,/) {
+		    print OUT "\"$value\"";
+		} else {
+		    print OUT "$value";
+		}
+	    }
+	}
     }
     print OUT "\n";
 }
 
 close($fh);
+
+print "Done!\n";
+exit(0);
